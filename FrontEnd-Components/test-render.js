e7682585a6ec8105ca651e3cc32564683c57a093
Jared Heeringa <jaredheeringa@icloud.com>
2021-11-09 20:49:57 -0600

    Starting creating partial templates and PUG templates

---
+++ FrontEnd-Components/test-render.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'ConvertedPUG'));
+
+app.get('/', (req, res) => {
+    res.render('MainLayout.pug', {
+        page_title: 'Minor Injury Report | FVSR',
+        user: {isLoggedIn: false, firstName: "James"},
+        copyright_current_year: new Date().getFullYear(),
+        pretty: true //This is for pretty HTML output (technically its deprecated, but it still works)
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
