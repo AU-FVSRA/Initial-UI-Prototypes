f90b5f5a7ddd7834393c69f1e75a1abd7ea80bcb
Jared Heeringa <jaredheeringa@icloud.com>
2021-11-15 22:26:21 -0600

    Fixed Pug structure

--- FrontEnd-Components/test-render.js
+++ FrontEnd-Components/test-render.js
@@ -7,14 +7,12 @@ app.set('views', path.join(__dirname, 'ConvertedPUG'));
 app.use(express.static(path.join(__dirname, '')));
 
 app.get('/', (req, res) => {
-    /*res.render('MainLayout.pug', {
+    res.render('MainLayout.pug', {
         page_title: 'Minor Injury Report | FVSR',
         user: {isLoggedIn: true, firstName: "James"},
         copyright_current_year: new Date().getFullYear(),
         pretty: true //This is for pretty HTML output (technically its deprecated, but it still works)
-    });*/
-    res.render('NavBar.pug')
-    res.render('F')
+    });
 });
 
 
