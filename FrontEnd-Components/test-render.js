5881ff4c0df61d67b75124a91a79f108eda5afa3
Jared Heeringa <jaredheeringa@icloud.com>
2021-11-15 21:47:08 -0600

    Completed PDRMA forms 03, 04, 04E

--- FrontEnd-Components/test-render.js
+++ FrontEnd-Components/test-render.js
@@ -4,14 +4,17 @@ const path = require('path');
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'ConvertedPUG'));
+app.use(express.static(path.join(__dirname, '')));
 
 app.get('/', (req, res) => {
-    res.render('MainLayout.pug', {
+    /*res.render('MainLayout.pug', {
         page_title: 'Minor Injury Report | FVSR',
         user: {isLoggedIn: true, firstName: "James"},
         copyright_current_year: new Date().getFullYear(),
         pretty: true //This is for pretty HTML output (technically its deprecated, but it still works)
-    });
+    });*/
+    res.render('NavBar.pug')
+    res.render('F')
 });
 
 
