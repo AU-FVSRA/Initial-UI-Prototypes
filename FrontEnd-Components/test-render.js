fb4a8a0574db0a7539a397e086c3536e7bcb2038
Jared Heeringa <jaredheeringa@icloud.com>
2021-11-26 17:23:27 -0600

    Added minor injury admin report view

--- FrontEnd-Components/test-render.js
+++ FrontEnd-Components/test-render.js
@@ -7,15 +7,21 @@ app.set('views', path.join(__dirname, 'ConvertedPUG'));
 app.use(express.static(path.join(__dirname, '')));
 
 app.get('/', (req, res) => {
+    // res.render('MainLayout.pug', {
+    //     page_title: 'Minor Injury Report | FVSR',
+    //     user: {isLoggedIn: true, firstName: "James"},
+    //     copyright_current_year: new Date().getFullYear(),
+    //     pretty: true //This is for pretty HTML output (technically its deprecated, but it still works)
+    // });
+
     res.render('MainLayout.pug', {
         page_title: 'Minor Injury Report | FVSR',
         user: {isLoggedIn: true, firstName: "James"},
+        minorInjuryLogReport: minorInjuryLogReport,
         copyright_current_year: new Date().getFullYear(),
-        pretty: true //This is for pretty HTML output (technically its deprecated, but it still works)
+        pretty: true,
     });
-    console.log(`%c hello`, "color: green; font-size: 20px;");
 });
-
 
 app.listen(3000, () => {
     console.table({
@@ -23,3 +29,72 @@ app.listen(3000, () => {
         "Environment": "Development"
     });
 });
+
+
+
+// Generate a random array of objects for minor injury reports
+minorInjuryLogReport = [
+    {
+        ID: 1,
+        ReportDate: "2020-01-01",
+        ReportTime: "12:00",
+        ProgramName: "Winter Basketball",
+        ProgramSeasonYear: "2020",
+    },
+    {
+        ID: 1,
+        ReportDate: "2020-01-01",
+        ReportTime: "12:00",
+        ProgramName: "Winter Basketball",
+        ProgramSeasonYear: "2020",
+    },
+    {
+        ID: 1,
+        ReportDate: "2020-01-01",
+        ReportTime: "12:00",
+        ProgramName: "Winter Basketball",
+        ProgramSeasonYear: "2020",
+    },
+    {
+        ID: 1,
+        ReportDate: "2020-01-01",
+        ReportTime: "12:00",
+        ProgramName: "Winter Basketball",
+        ProgramSeasonYear: "2020",
+    },
+    {
+        ID: 1,
+        ReportDate: "2020-01-01",
+        ReportTime: "12:00",
+        ProgramName: "Winter Basketball",
+        ProgramSeasonYear: "2020",
+    },
+    {
+        ID: 1,
+        ReportDate: "2020-01-01",
+        ReportTime: "12:00",
+        ProgramName: "Winter Basketball",
+        ProgramSeasonYear: "2020",
+    },
+    {
+        ID: 1,
+        ReportDate: "2020-01-01",
+        ReportTime: "12:00",
+        ProgramName: "Winter Basketball",
+        ProgramSeasonYear: "2020",
+    },
+    {
+        ID: 1,
+        ReportDate: "2020-01-01",
+        ReportTime: "12:00",
+        ProgramName: "Winter Basketball",
+        ProgramSeasonYear: "2020",
+    },
+    {
+        ID: 1,
+        ReportDate: "2020-01-01",
+        ReportTime: "12:00",
+        ProgramName: "Winter Basketball",
+        ProgramSeasonYear: "2020",
+    },
+];
